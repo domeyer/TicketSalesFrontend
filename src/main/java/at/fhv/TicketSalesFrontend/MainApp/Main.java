c780978ce9b26e4a6666b712773cced45a984fea
domeyer <do_meyer@icloud.com>
2019-10-23 15:32:25 +0200

    application setup

---
+++ src/main/java/at/fhv/TicketSalesFrontend/MainApp/Main.java
@@ -0,0 +1,24 @@
+package at.fhv.TicketSalesFrontend.MainApp;
+
+import at.fhv.TicketSalesFrontend.MainView.MainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainView view = new MainView();
+        Scene scene = new Scene(view.getView());
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(250);
+        primaryStage.setHeight(250);
+        primaryStage.show();
+
+    }
+}
---
+++ src/main/java/at/fhv/TicketSalesFrontend/MainView/MainView.java
@@ -0,0 +1,8 @@
+package at.fhv.TicketSalesFrontend.MainView;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class MainView extends FXMLView {
+
+
+}
